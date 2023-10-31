0da2fd31f42a03a38a277a85be0ebe363663bfb7
Dan Wilder <12650041+WilderDan@users.noreply.github.com>
2023-10-30 19:56:04 -0500

    Add Menu component

---
+++ src/components/menu/menu.js
@@ -0,0 +1,14 @@
+import styles from "./menu.css";
+
+const Menu = () => {
+  return /*htmL*/ `
+    <div class="${styles.menu}">
+        <h1>Our Menu</h1>
+        <div class="${styles.menuItemsContainer}">
+         
+        </div>
+    </div>
+    `;
+};
+
+export default Menu;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import styles from "./index.css";
 import Navbar from "./components/navbar/navbar.js";
 import Home from "./components/home/home.js";
+import Menu from "./components/menu/menu.js";
 
 const content = document.getElementById("content");
 
-content.innerHTML = Navbar() + Home();
+content.innerHTML = Menu();
